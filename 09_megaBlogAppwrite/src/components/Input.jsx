899e137c79950b3fb8e56c9a5572504ebcd8c579
Priti Parkhe <parkhepriti47@gmail.com>
2024-04-27 16:40:56 +0530

    add Login PostCard Signup

--- 09_megaBlogAppwrite/src/components/Input.jsx
+++ 09_megaBlogAppwrite/src/components/Input.jsx
@@ -7,26 +7,29 @@ const Input = React.forwardRef(
         className = " ",
         ...props
 
-    }, ref){
+    }, ref)
+    {
         const id = useId()
-        return 
+        
+        return (
         <div className='w-full'>
             { label && <label className='inline-block mb-1 pl-1' 
             htmlFor='{id'>
                 {label}
             </label>
             }
-        <input 
-        type={type}
-        className={` px-3 py-2 rounded-lg bg-white text-black outline-none
-         focus:bg-gray-50 duration-200 border borde-gray-200 w-full
-        ${className}`} 
-        ref={ref}
-        {...props}
-        id= {id}
-        />
+            <input 
+                type={type}
+                className={` px-3 py-2 rounded-lg bg-white text-black outline-none
+                focus:bg-gray-50 duration-200 border borde-gray-200 w-full
+                ${className}`} 
+                ref={ref}
+                {...props}
+                id= {id}
+                />
         
         </div>
+        )
     }
 )
 
